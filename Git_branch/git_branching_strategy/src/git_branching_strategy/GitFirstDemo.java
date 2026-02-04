48a3387c03b1d6154b1d80acd87d4be986ee3e72
om392 <omkar.pcf@gmail.com>
2026-02-04 19:38:15 +0530

    Updated GitFirstDemo

--- Git_branch/git_branching_strategy/src/git_branching_strategy/GitFirstDemo.java
+++ Git_branch/git_branching_strategy/src/git_branching_strategy/GitFirstDemo.java
@@ -3,6 +3,7 @@ package git_branching_strategy;
 public class GitFirstDemo {
 	public static void main(String [] args) {
 		System.out.println("This is git branching strategy demo. ");
+		System.out.println("This is hello world demo message .");
 	}
 
 }
