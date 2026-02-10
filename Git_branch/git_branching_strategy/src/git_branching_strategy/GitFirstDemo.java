9996ecdf25b2121febe6ede9f56ff4dc9d9d8110
om392 <omkar.pcf@gmail.com>
2026-02-10 14:34:19 +0530

    added a message

--- Git_branch/git_branching_strategy/src/git_branching_strategy/GitFirstDemo.java
+++ Git_branch/git_branching_strategy/src/git_branching_strategy/GitFirstDemo.java
@@ -2,8 +2,10 @@ package git_branching_strategy;
 
 public class GitFirstDemo {
 	public static void main(String [] args) {
+                String msg="Hello World From Git";
 		System.out.println("This is git branching strategy demo. ");
 		System.out.println("This is hello world demo message .");
+                System.out.println("Message is : "+msg)
 	}
 
 }
