d4c8091b5accbefd780f87db7ce239a7a2b9c91d
om392 <omkar.pcf@gmail.com>
2026-02-22 15:03:32 +0530

    m1 method added successfully

--- Git_branch/git_branching_strategy/src/git_branching_strategy/GitFirstDemo.java
+++ Git_branch/git_branching_strategy/src/git_branching_strategy/GitFirstDemo.java
@@ -1,7 +1,16 @@
 package git_branching_strategy;
 
 public class GitFirstDemo {
+   public void m1(int a , int b){
+          int result= a+b;
+  System.out.println("Addition result is : "+result);
+
+
+
+}
 	public static void main(String [] args) {
+         GitFirstDemo obj=new GitFirstDemo();
+                        obj.m1(5,15);
 		System.out.println("This is git branching strategy demo. ");
 		System.out.println("This is hello world demo message .");
 	}
